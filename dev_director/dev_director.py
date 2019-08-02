50d7e4efe376dbd92ab631ce513c22671fdf6e87
Niko Köser <koeserniko@gmail.com>
2019-08-02 02:52:53 +0200

    Implement first functions
    Implement the first functions that use the database as backend

--- dev_director/dev_director.py
+++ dev_director/dev_director.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sqlite3
 import subprocess
 import sys
 
@@ -35,64 +36,104 @@ def are_args_valid(args):
 class DevDirector:
 
     # Path to the database
-    db = "~/.config/dev_director/db/dd.sqlite"
+    conf_dir = "/home/niko/.config/dev_director/"
+    db = conf_dir + "db/dd.sqlite"
 
     def __init__(self, project_name, lang):
         self.project_name = project_name
         self.lang = lang
+        self.lang_id = 0
+        self.conn = None
+        self.cur = None
 
     def run(self):
-        # Check if provided language is supported
-        self.is_lang_supported()
+        # Check if directory already exists
+        self.__does_dir_exist()
 
-        # Create the project folder
-        self.create_project_folder()
+        # Connect to database
+        with sqlite3.connect(DevDirector.db) as self.conn:
+            if not self.conn:
+                print_error("Could not connect to database.")
+                sys.exit(1)
 
-        # Create language specific sub folders
-        self.create_sub_folders()
+            # Create cursor
+            self.cur = self.conn.cursor()
 
-    def does_dir_exist(self):
+            # Check if provided language is supported
+            self.__is_lang_supported()
+
+            # Create the project folder
+            self.__create_project_folder()
+
+            # Create language specific sub folders
+            self.__create_sub_folders()
+
+            # Create language specific files
+            self.__create_files()
+
+            # Copy template files
+            self.__copy_templates()
+
+    def __does_dir_exist(self):
         ''' Check if directory already exists. '''
 
         if os.path.exists(self.project_name):
-            print_error("Error: Directory already exists.")
+            print_error("Directory already exists.")
             sys.exit(2)
 
-    def is_lang_supported(self):
+    def __is_lang_supported(self):
         ''' Check if the provided language is supported. '''
 
-        langs = []
+        for lang_short in self.cur.execute('SELECT language_id, name_short FROM languages_short'):
+            if lang_short[1] == self.lang:
+                self.lang_id = lang_short[0]
+                return
 
-        if not self.lang in langs:
-            print_error("You have to specify a supported language.",
-                        ("Currently supported languages: " + str(langs)))
-            sys.exit(3)
+        langs = self.cur.execute(
+            'SELECT name_short FROM languages_short').fetchall()
 
-    def create_project_folder(self):
+        print_error("You have to specify a supported language.",
+                    ("Currently supported languages: " + str(langs)))
+        sys.exit(3)
+
+    def __create_project_folder(self):
         ''' Create the main project directory. '''
 
         # Create main directory
         print("Creating project folder...")
         subprocess.run(["mkdir", "-p", self.project_name])
 
-        # Create ReadMe
-        print("Creating ReadMe...")
-        cmd = 'echo "# ' + self.project_name + '" > ' + self.project_name + '/README.md'
-        subprocess.run(cmd, shell=True)
-
-    def create_sub_folders(self):
+    def __create_sub_folders(self):
         ''' Create sub folders depending on the specified language. '''
-
-        lang_folders = {}
 
         # Create subfolders
         print("Creating subfolders...")
 
-        for langs, folders in lang_folders.items():
-            if self.lang in langs:
-                for folder in folders:
-                    folder = self.project_name + '/' + folder
-                    subprocess.run(["mkdir", "-p", folder])
+        for sub_folder in self.cur.execute(
+                'SELECT relative_dest_path FROM folders WHERE language_id=?', (self.lang_id,)):
+            sub_folder = "./" + self.project_name + "/" + sub_folder[0]
+            subprocess.run(["mkdir", "-p", sub_folder])
+
+    def __create_files(self):
+        ''' Create language specific files. '''
+        # Create files
+        print("Creating files...")
+
+        for file in self.cur.execute(
+                'SELECT relative_dest_path FROM files WHERE language_id=? and is_template=?', (self.lang_id, 0,)):
+            file = "./" + self.project_name + "/" + file[0]
+            subprocess.run(["touch", file])
+
+    def __copy_templates(self):
+        ''' Create language specific files. '''
+        # Create files
+        print("Copying templates...")
+
+        for template in self.cur.execute(
+                'SELECT relative_dest_path, absolute_orig_path FROM files WHERE language_id=? and is_template=?', (self.lang_id, 1,)):
+            dest = self.project_name + "/" + template[0]
+            template = DevDirector.conf_dir + template[1]
+            subprocess.run(["cp", template, dest])
 
 
 def main():
