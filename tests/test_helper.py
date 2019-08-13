45d0f2331a8582864d3a18de68aed9261ce35a64
Niko Köser <koeserniko@gmail.com>
2019-08-13 19:17:27 +0200

    Add all remaining tests

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,23 +1,25 @@
 #!.env/bin/python3
 
-import unittest
+import pytest
 
 from src.helper import Helper
 
 
-class TestHelper(unittest.TestCase):
-    def test_format_err_msg_output(self):
-        self.assertEqual(
-            Helper.format_err_msg("TestCase"),
-            "Error: TestCase\n")
-        self.assertEqual(
-            Helper.format_err_msg("Test1", "Test2", "Test3"),
-            "Error: Test1\nTest2\nTest3\n")
+def test_format_err_msg_output():
+    assert Helper.format_err_msg(
+        "TestCase") == "Error: TestCase\n", "Test failed"
+    assert (Helper.format_err_msg(
+        "Test1", "Test2", "Test3") == "Error: Test1\nTest2\nTest3\n",
+        "Test failed")
 
-    def test_format_err_msg_values(self):
-        self.assertRaises(ValueError, Helper.format_err_msg)
 
-    def test_format_err_msg_types(self):
-        self.assertRaises(TypeError, Helper.format_err_msg, True)
-        self.assertRaises(TypeError, Helper.format_err_msg, 123)
-        self.assertRaises(TypeError, Helper.format_err_msg, "Test", 123)
+def test_format_err_msg_values():
+    with pytest.raises(ValueError):
+        Helper.format_err_msg()
+
+
+def test_format_err_msg_types():
+    with pytest.raises(TypeError):
+        Helper.format_err_msg(True)
+        Helper.format_err_msg(123)
+        Helper.format_err_msg("Test", 123)
