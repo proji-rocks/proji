befdf961a00e9ff0dcf12f6576e02795af9251b6
Niko Köser <koeserniko@gmail.com>
2019-08-13 19:07:08 +0200

    Make members private and add init checks
    Add '__' prefix to member variables. Add argument checks to __init__ method.

--- src/create_project.py
+++ src/create_project.py
@@ -5,28 +5,32 @@ import sqlite3
 import subprocess
 import sys
 
-from helper import Helper
+from src.helper import Helper
 
 
 class CreateProject:
 
     # Path to the database
-    conf_dir = "/home/niko/.config/create_project/"
-    db = conf_dir + "db/cp.sqlite"
+    __conf_dir = "/home/niko/.config/create_project/"
+    __db = __conf_dir + "db/cp.sqlite"
 
     def __init__(self, project_name, lang):
-        self.project_name = project_name
-        self.lang = lang
-        self.lang_id = 0
-        self.conn = None
-        self.cur = None
+        if type(project_name) != str:
+            raise TypeError("Project name must be a string.")
+        if type(lang) != str:
+            raise TypeError("Language name must be a string.")
+        self.__project_name = project_name
+        self.__lang = lang
+        self.__lang_id = 0
+        self.__conn = None
+        self.__cur = None
 
     def run(self):
         # Check if directory already exists
         self.__does_dir_exist()
 
         # Connect to database
-        with sqlite3.connect(CreateProject.db) as self.conn:
+        with sqlite3.connect(CreateProject.__db) as self.conn:
             if not self.conn:
                 err_msg = Helper.format_err_msg(
                     "Could not connect to database.")
@@ -34,7 +38,7 @@ class CreateProject:
                 sys.exit(1)
 
             # Create cursor
-            self.cur = self.conn.cursor()
+            self.__cur = self.conn.cursor()
 
             # Check if provided language is supported
             self.__is_lang_supported()
@@ -54,7 +58,7 @@ class CreateProject:
     def __does_dir_exist(self):
         ''' Check if directory already exists. '''
 
-        if os.path.exists(self.project_name):
+        if os.path.exists(self.__project_name):
             err_msg = Helper.format_err_msg("Directory already exists.")
             print(err_msg)
             sys.exit(2)
@@ -62,18 +66,18 @@ class CreateProject:
     def __is_lang_supported(self):
         ''' Check if the provided language is supported. '''
 
-        for lang_short in self.cur.execute('''
+        for lang_short in self.__cur.execute('''
                                             SELECT
                                                 language_id,
                                                 name_short
                                             FROM
                                                 languages_short
                                             '''):
-            if lang_short[1] == self.lang:
+            if lang_short[1] == self.__lang:
                 self.lang_id = lang_short[0]
                 return
 
-        langs = self.cur.execute(
+        langs = self.__cur.execute(
             'SELECT name_short FROM languages_short').fetchall()
 
         err_msg = Helper.format_err_msg(
@@ -87,7 +91,7 @@ class CreateProject:
 
         # Create main directory
         print("Creating project folder...")
-        subprocess.run(["mkdir", "-p", self.project_name])
+        subprocess.run(["mkdir", "-p", self.__project_name])
 
     def __create_sub_folders(self):
         ''' Create sub folders depending on the specified language. '''
@@ -95,7 +99,7 @@ class CreateProject:
         # Create subfolders
         print("Creating subfolders...")
 
-        for sub_folder in self.cur.execute(
+        for sub_folder in self.__cur.execute(
             '''
             SELECT
                 relative_dest_path
@@ -106,7 +110,7 @@ class CreateProject:
             ''',
                 (self.lang_id,)):
 
-            sub_folder = "./" + self.project_name + "/" + sub_folder[0]
+            sub_folder = "./" + self.__project_name + "/" + sub_folder[0]
             subprocess.run(["mkdir", "-p", sub_folder])
 
     def __create_files(self):
@@ -114,7 +118,7 @@ class CreateProject:
         # Create files
         print("Creating files...")
 
-        for file in self.cur.execute(
+        for file in self.__cur.execute(
             '''
             SELECT
                 relative_dest_path
@@ -126,7 +130,7 @@ class CreateProject:
             ''',
                 (self.lang_id, 0,)):
 
-            file = "./" + self.project_name + "/" + file[0]
+            file = "./" + self.__project_name + "/" + file[0]
             subprocess.run(["touch", file])
 
     def __copy_templates(self):
@@ -134,7 +138,7 @@ class CreateProject:
         # Create files
         print("Copying templates...")
 
-        for template in self.cur.execute(
+        for template in self.__cur.execute(
             '''
             SELECT
                 relative_dest_path,
@@ -146,6 +150,6 @@ class CreateProject:
                 is_template=?''',
                 (self.lang_id, 1,)):
 
-            dest = self.project_name + "/" + template[0]
-            template = CreateProject.conf_dir + template[1]
+            dest = self.__project_name + "/" + template[0]
+            template = CreateProject.__conf_dir + template[1]
             subprocess.run(["cp", template, dest])
