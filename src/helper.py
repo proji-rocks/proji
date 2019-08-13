26fdfce4f6bf8d2d4bd89e808f10970008b21619
Niko Köser <koeserniko@gmail.com>
2019-08-13 17:57:25 +0200

    Create helper class

--- src/helper.py
+++ src/helper.py
@@ -5,28 +5,31 @@
 import sys
 
 
-def print_error(*err_msg):
-    ''' Print an error. '''
+class Helper:
+    @staticmethod
+    def print_error(*err_msg):
+        ''' Print an error. '''
 
-    err_out = "Error: "
+        err_out = "Error: "
 
-    for err in err_msg:
-        err_out += (err + '\n')
+        for err in err_msg:
+            err_out += (err + '\n')
 
-    print(err_out)
+        print(err_out)
 
+    @staticmethod
+    def are_args_valid(args):
+        ''' Check number of cli arguments. '''
 
-def are_args_valid(args):
-    ''' Check number of cli arguments. '''
+        if len(args) != 3:
+            Helper.print_error(
+                "Missing arguments.",
+                "Syntax: create_project <projectname> <language>")
+            sys.exit(1)
 
-    if len(args) != 3:
-        print_error("Missing arguments.",
-                    "Syntax: create_project <projectname> <language>")
-        sys.exit(1)
-
-    if not str(sys.argv[1]).strip():
-        print_error("Error: Projectname needs to be specified.")
-        sys.exit(2)
-    if not str(sys.argv[2]).strip():
-        print_error("Error: Language needs to be specified.")
-        sys.exit(3)
+        if not str(sys.argv[1]).strip():
+            Helper.print_error("Error: Projectname needs to be specified.")
+            sys.exit(2)
+        if not str(sys.argv[2]).strip():
+            Helper.print_error("Error: Language needs to be specified.")
+            sys.exit(3)
