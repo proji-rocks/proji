71af7122c8d25edb4cc87b4c0110aaaafc425e7a
Niko Köser <koeserniko@gmail.com>
2019-08-13 17:46:01 +0200

    Add main function

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,23 @@
 #!.env/bin/python3
 
+import sys
+
+from create_project import CreateProject
+from helper import are_args_valid
+
+
+def main():
+    ''' Main function. '''
+    # Check cli args
+    are_args_valid(sys.argv)
+
+    # Define name and language
+    project_name = sys.argv[1]
+    lang = str(sys.argv[2]).lower()
+
+    cp = CreateProject(project_name, lang)
+    cp.run()
+
 
 if __name__ == "__main__":
-    pass
+    main()
