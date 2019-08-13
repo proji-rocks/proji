d6c04b67ed5337889a3e46022018f029b3c69e58
Niko Köser <koeserniko@gmail.com>
2019-08-13 17:29:47 +0200

    Add helper functions file

---
+++ src/helper.py
@@ -0,0 +1,30 @@
+#!.env/bin/python3
+
+'''A file with helper functions.'''
+
+
+def print_error(*err_msg):
+    ''' Print an error. '''
+
+    err_out = "Error: "
+
+    for err in err_msg:
+        err_out += (err + '\n')
+
+    print(err_out)
+
+
+def are_args_valid(args):
+    ''' Check number of cli arguments. '''
+
+    if len(args) != 3:
+        print_error("Missing arguments.",
+                    "Syntax: create_project <projectname> <language>")
+        sys.exit(1)
+
+    if not str(sys.argv[1]).strip():
+        print_error("Error: Projectname needs to be specified.")
+        sys.exit(2)
+    if not str(sys.argv[2]).strip():
+        print_error("Error: Language needs to be specified.")
+        sys.exit(3)
