ff06a9ff7f731cfd63f28cb3cd1cca79b35f6cd7
Niko Köser <koeserniko@gmail.com>
2019-08-14 17:38:18 +0200

    Test lang_supported
    Test the CreateProject method lang_supported.

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -1,8 +1,33 @@
 #!.env/bin/python3
 
+import sqlite3
+
 import pytest
 
-from src.create_project import CreateProject
+from create_project.create_project import CreateProject
+
+
+@pytest.fixture
+def exmpl_cp():
+    # Db Connection
+    conn = sqlite3.connect(CreateProject.get_db_path())
+
+    # List of instances
+    cps = []
+
+    cps.append(CreateProject("TestProject1", "cpp"))
+    cps.append(CreateProject("TestProject2", "py"))
+    cps.append(CreateProject("TestProject3", "js"))
+    cps.append(CreateProject("TestProject4", "x"))
+    cps.append(CreateProject("TestProject5", "fk"))
+    cps.append(CreateProject("TestProject6", ""))
+
+    # Add connection to cp instances
+    for cp in cps:
+        cp._conn = conn
+        cp._cur = cp._conn.cursor()
+
+    return cps
 
 
 def test_init():
@@ -19,3 +44,15 @@ def test_init_types():
         CreateProject(True, False)
         CreateProject("Test", True)
         CreateProject(True, "Test")
+
+
+def test_lang_supported(exmpl_cp):
+    # Supported languages
+    assert exmpl_cp[0]._is_lang_supported()
+    assert exmpl_cp[1]._is_lang_supported()
+    assert exmpl_cp[2]._is_lang_supported()
+
+    # Not supported languages
+    assert not exmpl_cp[3]._is_lang_supported()
+    assert not exmpl_cp[4]._is_lang_supported()
+    assert not exmpl_cp[5]._is_lang_supported()
