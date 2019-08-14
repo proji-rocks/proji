7da96cd05d535026ad2db6c5c0ead23725e6ab75
Niko Köser <koeserniko@gmail.com>
2019-08-14 18:05:39 +0200

    Split cps fixture
    Split cps fixture in two fixtures. One for valid and one for invalid cp instances.

--- tests/test_create_project.py
+++ tests/test_create_project.py
@@ -7,17 +7,55 @@ import pytest
 from create_project.create_project import CreateProject
 
 
+'''
+
+FIXTURES
+
+'''
+
+
 @pytest.fixture
-def exmpl_cp():
+def valid_db_conn():
     # Db Connection
-    conn = sqlite3.connect(CreateProject.get_db_path())
+    return sqlite3.connect(CreateProject.get_db_path())
+
+
+@pytest.fixture
+def invalid_db_conn():
+    # Db Connection
+    return sqlite3.connect("i_dont_exist.sqlite")
+
+
+@pytest.fixture
+def valid_cps(valid_db_conn):
+    # Db Connection
+    conn = valid_db_conn
 
     # List of instances
     cps = []
 
+    # Valid
     cps.append(CreateProject("TestProject1", "cpp"))
     cps.append(CreateProject("TestProject2", "py"))
     cps.append(CreateProject("TestProject3", "js"))
+
+    # Add connection to cp instances
+    for cp in cps:
+        cp._conn = conn
+        cp._cur = cp._conn.cursor()
+
+    return cps
+
+
+@pytest.fixture
+def invalid_cps(valid_db_conn):
+    # Db Connection
+    conn = valid_db_conn
+
+    # List of instances
+    cps = []
+
+    # Invalid
     cps.append(CreateProject("TestProject4", "x"))
     cps.append(CreateProject("TestProject5", "fk"))
     cps.append(CreateProject("TestProject6", ""))
@@ -28,6 +66,13 @@ def exmpl_cp():
         cp._cur = cp._conn.cursor()
 
     return cps
+
+
+'''
+
+TESTS
+
+'''
 
 
 def test_init():
@@ -45,14 +90,16 @@ def test_init_types():
         CreateProject("Test", True)
         CreateProject(True, "Test")
 
+def
 
-def test_lang_supported(exmpl_cp):
+
+def test_lang_supported(valid_cps, invalid_cps):
     # Supported languages
-    assert exmpl_cp[0]._is_lang_supported()
-    assert exmpl_cp[1]._is_lang_supported()
-    assert exmpl_cp[2]._is_lang_supported()
+    assert valid_cps[0]._is_lang_supported()
+    assert valid_cps[1]._is_lang_supported()
+    assert valid_cps[2]._is_lang_supported()
 
     # Not supported languages
-    assert not exmpl_cp[3]._is_lang_supported()
-    assert not exmpl_cp[4]._is_lang_supported()
-    assert not exmpl_cp[5]._is_lang_supported()
+    assert not invalid_cps[0]._is_lang_supported()
+    assert not invalid_cps[1]._is_lang_supported()
+    assert not invalid_cps[2]._is_lang_supported()
