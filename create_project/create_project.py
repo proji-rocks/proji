673f87ba43adca7ba391b37c81c61746e297a7ff
Niko Köser <koeserniko@gmail.com>
2019-08-14 17:24:18 +0200

    Change method and member prefix Change method and member prefix from '__' to '_'. Methods were not callable from testfiles.

--- create_project/create_project.py
+++ create_project/create_project.py
@@ -19,15 +19,15 @@ class CreateProject:
             raise TypeError("Project name must be a string.")
         if type(lang) != str:
             raise TypeError("Language name must be a string.")
-        self.__project_name = project_name
-        self.__lang = lang
-        self.__lang_id = 0
-        self.__conn = None
-        self.__cur = None
+        self._project_name = project_name
+        self._lang = lang
+        self._lang_id = 0
+        self._conn = None
+        self._cur = None
 
     def run(self):
         # Check if directory already exists
-        if self.__does_dir_exist():
+        if self._does_dir_exist():
             return 1
 
         # Connect to database
@@ -39,66 +39,66 @@ class CreateProject:
                 return 2
 
             # Create cursor
-            self.__cur = self.conn.cursor()
+            self._cur = self.conn.cursor()
 
-            if not self.__cur:
+            if not self._cur:
                 return 3
 
             # Check if provided language is supported
-            if not self.__is_lang_supported():
+            if not self._is_lang_supported():
                 return 4
 
             # Create the project folder
-            if not self.__create_project_folder():
+            if not self._create_project_folder():
                 return 5
 
             # Create language specific sub folders
-            if not self.__create_sub_folders():
+            if not self._create_sub_folders():
                 return 6
 
             # Create language specific files
-            if not self.__create_files():
+            if not self._create_files():
                 return 7
 
             # Copy template files
-            if not self.__copy_templates():
+            if not self._copy_templates():
                 return 8
         return 0
 
     def get_project_name(self):
         ''' Get the project name. '''
-        return self.__project_name
+        return self._project_name
 
     def get_language(self):
         ''' Get the language. '''
-        return self.__lang
+        return self._lang
 
-    def __does_dir_exist(self):
+    def _does_dir_exist(self):
         ''' Check if directory already exists. '''
 
-        if os.path.exists(self.__project_name):
+        if os.path.exists(self._project_name):
             err_msg = Helper.format_err_msg("Directory already exists.")
             print(err_msg)
             return True
         return False
 
-    def __is_lang_supported(self):
+    def _is_lang_supported(self):
         ''' Check if the provided language is supported. '''
 
-        for lang_short in self.__cur.execute('''
+        for lang_short in self._cur.execute('''
                                             SELECT
                                                 language_id,
                                                 name_short
                                             FROM
                                                 languages_short
                                             '''):
-            if lang_short[1] == self.__lang:
+            if lang_short[1] == self._lang:
                 # Found language
                 self.lang_id = lang_short[0]
                 return True
 
         # Language not supported
-        langs = self.__cur.execute(
+        langs = self._cur.execute(
             'SELECT name_short FROM languages_short').fetchall()
 
         err_msg = Helper.format_err_msg(
@@ -107,26 +107,26 @@ class CreateProject:
         print(err_msg)
         return False
 
-    def __create_project_folder(self):
+    def _create_project_folder(self):
         ''' Create the main project directory. '''
 
         # Create main directory
         print("Creating project folder...")
         try:
-            subprocess.run(["mkdir", "-p", self.__project_name], timeout=10.0)
+            subprocess.run(["mkdir", "-p", self._project_name], timeout=10.0)
         except (subprocess.CalledProcessError, TimeoutError) as err:
             Helper.format_err_msg("Couldn't create project folder.", err)
             return False
         return True
 
-    def __create_sub_folders(self):
+    def _create_sub_folders(self):
         ''' Create sub folders depending on the specified language. '''
 
         # Create subfolders
         print("Creating subfolders...")
 
         try:
-            for sub_folder in self.__cur.execute(
+            for sub_folder in self._cur.execute(
                 '''
                 SELECT
                     relative_dest_path
@@ -137,20 +137,20 @@ class CreateProject:
                 ''',
                     (self.lang_id,)):
 
-                sub_folder = "./" + self.__project_name + "/" + sub_folder[0]
+                sub_folder = "./" + self._project_name + "/" + sub_folder[0]
                 subprocess.run(["mkdir", "-p", sub_folder], timeout=10.0)
         except (subprocess.CalledProcessError, TimeoutError) as err:
             Helper.format_err_msg("Couldn't create sub folders.", err)
             return False
         return True
 
-    def __create_files(self):
+    def _create_files(self):
         ''' Create language specific files. '''
         # Create files
         print("Creating files...")
 
         try:
-            for file in self.__cur.execute(
+            for file in self._cur.execute(
                 '''
                 SELECT
                     relative_dest_path
@@ -162,20 +162,20 @@ class CreateProject:
                 ''',
                     (self.lang_id, 0,)):
 
-                file = "./" + self.__project_name + "/" + file[0]
+                file = "./" + self._project_name + "/" + file[0]
                 subprocess.run(["touch", file], timeout=10.0)
         except (subprocess.CalledProcessError, TimeoutError) as err:
             Helper.format_err_msg("Couldn't create file.", err)
             return False
         return True
 
-    def __copy_templates(self):
+    def _copy_templates(self):
         ''' Create language specific files. '''
         # Create files
         print("Copying templates...")
 
         try:
-            for template in self.__cur.execute(
+            for template in self._cur.execute(
                 '''
                 SELECT
                     relative_dest_path,
@@ -185,9 +185,9 @@ class CreateProject:
                 WHERE
                     language_id=? and
                     is_template=?''',
-                    (self.lang_id, 1,)):
+                    (self._lang_id, 1,)):
 
-                dest = self.__project_name + "/" + template[0]
+                dest = self._project_name + "/" + template[0]
                 template = CreateProject.__conf_dir + template[1]
                 subprocess.run(["cp", template, dest])
         except (subprocess.CalledProcessError, TimeoutError) as err:
