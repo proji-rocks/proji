911bc007f256015600e01b8039271ff4dd7dc7e5
Niko Köser <koeserniko@gmail.com>
2019-08-15 15:56:55 +0200

    Fix always true test
    Test was always true because of parenthese. Caused by black formatting.

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -6,12 +6,8 @@ from create_project.helper import Helper
 
 
 def test_format_err_msg_output():
-    assert Helper.format_err_msg("TestCase") == "Error: TestCase\n", "Test failed"
-    assert (
-        Helper.format_err_msg("Test1", "Test2", "Test3")
-        == "Error: Test1\nTest2\nTest3\n",
-        "Test failed",
-    )
+    assert Helper.format_err_msg("TestCase") == "Error: TestCase\n"
+    assert Helper.format_err_msg("T1", "T2", "T3") == "Error: T1\nT2\nT3\n"
 
 
 def test_format_err_msg_values():
