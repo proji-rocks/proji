8462df9d08a780b2bc30f2c00b61b6bf94c63b73
Niko Köser <koeserniko@gmail.com>
2019-08-20 23:34:47 +0200

    Add new templates
    Add new and update old template files.

---
+++ templates/template.c
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Title:
+ * Project:
+ * Author:
+ * Description:
+ ******************************************************************************/
+
+/* Includes */
+#include <stdio.h>
+
+/* Declarations */
+
+/* Main function */
+int main() {
+    printf("Hello world!\n");
+    return 0;
+}
+
+/* Definitions */
---
+++ templates/template.cpp
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Title:
+ * Project:
+ * Author:
+ * Description:
+ ******************************************************************************/
+
+/* Includes */
+#include <iostream>
+
+/* Declarations */
+
+/* Main function */
+int main() {
+    std::cout << "Hello world!\n";
+}
+
+/* Definitions */
