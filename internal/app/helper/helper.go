bbf6c9afd42c440ae7a0ea546fc925ff39939908
Niko Köser <koeserniko@gmail.com>
2019-08-21 18:02:56 +0200

    Add ParseArgs function
    Move function ParseArgs from old package construct to helper package. Checks and parses the cli arguments.

--- internal/app/helper/helper.go
+++ internal/app/helper/helper.go
@@ -1,10 +1,26 @@
 package helper
 
-import "strings"
+import (
+	"errors"
+	"os"
+	"strings"
+)
 
 // ProjectHeader returns an individual graphical header for a project
 func ProjectHeader(projectName string) string {
 	separatorLine := strings.Repeat("#", 50) + "\n"
 	projectLine := "# " + projectName + "\n"
 	return (separatorLine + "#\n" + projectLine + "#\n" + separatorLine)
+}
+
+// ParseArgs parses the cli arguments to the needed data - the extension and the project names.
+// AreArgsValid() should be run before this function.
+func ParseArgs() (string, []string, error) {
+	args := os.Args[1:]
+
+	if len(args) < 2 {
+		return "", []string{}, errors.New("Insufficient number of cli arguments.\nSyntax: create_project <extension> <project> [projects]")
+	}
+
+	return args[0], args[1:], nil
 }
