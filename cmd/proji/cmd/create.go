a4f5464f5ad90a1c51024afc64d22971658b5079
Niko Köser <koeserniko@gmail.com>
2019-08-24 23:43:19 +0200

    Remove flags comment block
    Remove the comment block containing information about flags.

--- cmd/proji/cmd/create.go
+++ cmd/proji/cmd/create.go
@@ -23,14 +23,4 @@ var createCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
