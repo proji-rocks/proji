f5c10b9df59a1481e7de5e2c53fc5feb5f5002ad
Niko Köser <koeserniko@gmail.com>
2019-08-27 01:32:44 +0200

    Make class command not executable
    Make the class command not executable. A sub command like 'add' has to be called.

--- cmd/proji/cmd/class.go
+++ cmd/proji/cmd/class.go
@@ -1,24 +1,14 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var classCmd = &cobra.Command{
-	Use:   "class NAME",
-	Short: "Add a new class to the database",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return fmt.Errorf("Missing class name")
-		}
-
-		fmt.Println("Added new class " + args[0])
-		return nil
-	},
+	Use:   "class COMMAND",
+	Short: "Work on proji classes",
 }
 
 func init() {
-	addCmd.AddCommand(classCmd)
+	rootCmd.AddCommand(classCmd)
 }
