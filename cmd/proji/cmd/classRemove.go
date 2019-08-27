ad2f62a4eaab1808c6d5b82f557bed3eb8c718e9
Niko Köser <koeserniko@gmail.com>
2019-08-27 02:37:03 +0200

    Add new class subcommand 'remove'
    The subcommand is a direct subcommand of the class command. Remove enables the user to remove existing classes.

---
+++ cmd/proji/cmd/classRemove.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/nikoksr/proji/internal/app/proji/class"
+	"github.com/spf13/cobra"
+)
+
+var classRemoveCmd = &cobra.Command{
+	Use:   "remove CLASS [CLASS...]",
+	Short: "Remove existing classes",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("Missing class name")
+		}
+
+		for _, className := range args {
+			err := class.RemoveClass(className)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	},
+}
+
+func init() {
+	classCmd.AddCommand(classRemoveCmd)
+}
