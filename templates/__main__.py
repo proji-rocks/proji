8f24b63668fb77d2f03715e2812e659bd1de68a5
Niko Köser <koeserniko@gmail.com>
2019-08-29 16:45:44 +0200

    Remove templates folder from remote
    Remove the templates folder from git but not from the filesystem.

--- templates/__main__.py
+++
@@ -1,5 +0,0 @@
-#!.env/bin/python3
-
-
-if __name__ == '__main__':
-    pass
--- templates/index.php
+++
@@ -1,9 +0,0 @@
-<?php    
-    // load up your config file
-    require_once("/path/to/resources/config.php");
-?>
-<div id="container">
-    <div id="content">
-        <p>Hello World!</p>
-    </div>
-</div>
