cd3a02c7b9f744bb0d05bdbf87f9b91b96b32ae2
Niko Köser <koeserniko@gmail.com>
2019-08-30 16:21:30 +0200

    Refactor db scripts Split db script in three scripts. One for creating the tables, one for inserting test data and for creating the indexes.
    Update table and column names. Added a new table group 'global'. There are now 'folder_'- and 'global_'- 'file' / 'folder' / 'script' tables respectively. Now globals aren't defined anymore by a missing class_id. Makes it easier to understand and solves problems with indexes (see #38).
    
    Rename table 'project_class' to 'class'.
    
    Rename columns:
        - 'class_name' -> 'name'
        - 'target_path' -> 'target'
        - 'template_name_ -> 'template'
        - 'script_name' -> 'name'

---
+++ scripts/insert_test_data.sql
@@ -0,0 +1,83 @@
+--
+-- INSERT INTO TABLES
+--
+INSERT INTO
+  project_status(project_status, comment)
+VALUES
+  (
+    "active",
+    "I'm actively working on this project."
+  ),
+  (
+    "inactive",
+    "I stopped working on this project for now."
+  ),
+  (
+    "done",
+    "I finished this project. There is nothing to do."
+  ),
+  (
+    "dead",
+    "This project is dead. I'm not planning to work on it anytime soon."
+  );
+--
+INSERT INTO
+  class('name')
+VALUES
+  ("c-plus-plus"),
+  ("python");
+--
+INSERT INTO
+  class_label(class_id, label)
+VALUES
+  (1, "cpp"),
+  (1, "c++"),
+  (1, "cc"),
+  (2, "python"),
+  (2, "py");
+--
+INSERT INTO
+  class_folder(class_id, 'target', template)
+VALUES
+  (1, "build/debug", NULL),
+  (1, "build/release", NULL),
+  (1, "doc", NULL),
+  (1, "include", NULL),
+  (1, "lib", NULL),
+  (1, "src", NULL),
+  (1, "test", NULL),
+  (2, "src", NULL),
+  (2, "doc", NULL),
+  (2, "test", NULL);
+--
+INSERT INTO
+  class_file(class_id, 'target', template)
+VALUES
+  (1, "src/main.cpp", "main.cpp"),
+  (
+    1,
+    "CMakeLists.txt",
+    "CMakeLists-cpp.txt"
+  ),
+  (2, "src/__main__.py", NULL),
+  (2, "src/__init__.py", NULL);
+--
+INSERT INTO
+  class_script(class_id, 'name', run_as_sudo)
+VALUES
+  (2, "init_virtualenv.sh", 0);
+--
+INSERT INTO
+  global_folder('target', template)
+VALUES
+  (".vscode", "vscode");
+--
+INSERT INTO
+  global_file('target', template)
+VALUES
+  (".gitignore", "gitignore");
+--
+INSERT INTO
+  global_script('name', run_as_sudo)
+VALUES
+  ("init_git.sh", 0);
