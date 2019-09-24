5306fb4e7c4e2e015cf2c317598693abd6acf98a
Niko Köser <koeserniko@gmail.com>
2019-09-24 15:19:11 +0200

    Refactor the storage interface and sqlite package Prior to this change, the sqlite package didn't contain much functionality. Pretty much an empty and dead file.
    This change fills the sqlite package with all the code that's necessary
    to read and write classes and projects from and to the sqlite3 database.
    All the database interactions that were previously done other packages
    are now handeld in this package. The Storage interface serves as
    representation for the necessary behaviour of a storage related package.
    It defines the necessary functions to save, load, remove classes and
    projects.

--- pkg/proji/storage/sqlite/sqlite.go
+++ pkg/proji/storage/sqlite/sqlite.go
@@ -4,20 +4,21 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
+	"time"
 
-	"github.com/nikoksr/proji/internal/app/proji/class"
-	"github.com/nikoksr/proji/internal/app/storage"
+	"github.com/mattn/go-sqlite3"
+	"github.com/nikoksr/proji/pkg/proji/storage"
 )
 
 // Sqlite represents a sqlite connection.
 type sqlite struct {
-	DB *sql.DB
-	Tx *sql.Tx
+	db *sql.DB
+	tx *sql.Tx
 }
 
 // New creates a new connection to a sqlite database.
-func New(name string) (storage.Service, error) {
-	db, err := sql.Open("sqlite3", name)
+func New(path string) (storage.Service, error) {
+	db, err := sql.Open("sqlite3", path)
 	if err != nil {
 		return nil, err
 	}
@@ -30,81 +31,110 @@ func New(name string) (storage.Service, error) {
 	return &sqlite{db, nil}, nil
 }
 
-// Close the sqlite connection.
 func (s *sqlite) Close() error {
-	return s.Close()
+	return s.db.Close()
 }
 
-func (s *sqlite) Save(c *class.Class) error {
-	if err := s.saveName(c.Name); err != nil {
+func (s *sqlite) SaveClass(class *storage.Class) error {
+	if err := s.saveName(class.Name); err != nil {
 		return err
 	}
 
-	tx, err := s.DB.Begin()
+	// After saving the name, the class gets a unique id.
+	id, err := s.LoadClassID(class.Name)
+	if err != nil {
+		if e := s.cancelSave(class.Name); e != nil {
+			return e
+		}
+		return err
+	}
+	class.ID = id
+	tx, err := s.db.Begin()
 	if err != nil {
 		return err
 	}
-	s.Tx = tx
+	s.tx = tx
 
-	if err := s.saveLabels(c); err != nil {
+	if err := s.saveLabels(class); err != nil {
+		if e := s.cancelSave(class.Name); e != nil {
+			return e
+		}
 		return err
 	}
 
-	if err := s.saveFolders(c); err != nil {
+	if err := s.saveFolders(class); err != nil {
+		if e := s.cancelSave(class.Name); e != nil {
+			return e
+		}
 		return err
 	}
 
-	if err := s.saveFiles(c); err != nil {
+	if err := s.saveFiles(class); err != nil {
+		if e := s.cancelSave(class.Name); e != nil {
+			return e
+		}
 		return err
 	}
 
-	if err := s.saveScripts(c); err != nil {
+	if err := s.saveScripts(class); err != nil {
+		if e := s.cancelSave(class.Name); e != nil {
+			return e
+		}
 		return err
 	}
 
-	return s.Tx.Commit()
+	return s.tx.Commit()
+}
+
+func (s *sqlite) cancelSave(className string) error {
+	if s.tx != nil {
+		if err := s.tx.Rollback(); err != nil {
+			return err
+		}
+	}
+	return s.RemoveClass(className)
 }
 
 func (s *sqlite) saveName(name string) error {
 	query := "INSERT INTO class(name) VALUES(?)"
 	name = strings.ToLower(name)
-	_, err := s.DB.Exec(query, name)
+	_, err := s.db.Exec(query, name)
 	return err
 }
 
-func (s *sqlite) saveLabels(c *class.Class) error {
-	if s.Tx == nil {
+func (s *sqlite) saveLabels(class *storage.Class) error {
+	if s.tx == nil {
 		return fmt.Errorf("no open transaction")
 	}
 
 	query := "INSERT INTO class_label(class_id, label) VALUES(?, ?)"
-	stmt, err := s.Tx.Prepare(query)
+	stmt, err := s.tx.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	for _, label := range c.Labels {
-		if _, err = stmt.Exec(c.ID, strings.ToLower(label)); err != nil {
+	for _, label := range class.Labels {
+		if _, err = stmt.Exec(class.ID, strings.ToLower(label)); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (s *sqlite) saveFolders(c *class.Class) error {
+func (s *sqlite) saveFolders(class *storage.Class) error {
 	query := "INSERT INTO class_folder(class_id, target, template) VALUES(?, ?, ?)"
-	stmt, err := s.Tx.Prepare(query)
+	stmt, err := s.tx.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	for target, template := range c.Folders {
+	for target, template := range class.Folders {
 		if len(template) > 0 {
-			_, err = stmt.Exec(c.ID, target, template)
+			_, err = stmt.Exec(class.ID, target, template)
 		} else {
-			_, err = stmt.Exec(c.ID, target, nil)
+			_, err = stmt.Exec(class.ID, target, nil)
 		}
 		if err != nil {
 			return err
@@ -113,19 +143,19 @@ func (s *sqlite) saveFolders(c *class.Class) error {
 	return nil
 }
 
-func (s *sqlite) saveFiles(c *class.Class) error {
+func (s *sqlite) saveFiles(class *storage.Class) error {
 	query := "INSERT INTO class_file(class_id, target, template) VALUES(?, ?, ?)"
-	stmt, err := s.Tx.Prepare(query)
+	stmt, err := s.tx.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	for target, template := range c.Files {
+	for target, template := range class.Files {
 		if len(template) > 0 {
-			_, err = stmt.Exec(c.ID, target, template)
+			_, err = stmt.Exec(class.ID, target, template)
 		} else {
-			_, err = stmt.Exec(c.ID, target, nil)
+			_, err = stmt.Exec(class.ID, target, nil)
 		}
 		if err != nil {
 			return err
@@ -134,19 +164,19 @@ func (s *sqlite) saveFiles(c *class.Class) error {
 	return nil
 }
 
-func (s *sqlite) saveScripts(c *class.Class) error {
+func (s *sqlite) saveScripts(class *storage.Class) error {
 	query := "INSERT INTO class_script(class_id, name, run_as_sudo) VALUES(?, ?, ?)"
-	stmt, err := s.Tx.Prepare(query)
+	stmt, err := s.tx.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	for script, asSudo := range c.Scripts {
+	for script, asSudo := range class.Scripts {
 		if asSudo {
-			_, err = stmt.Exec(c.ID, script, 1)
+			_, err = stmt.Exec(class.ID, script, 1)
 		} else {
-			_, err = stmt.Exec(c.ID, script, 0)
+			_, err = stmt.Exec(class.ID, script, 0)
 		}
 		if err != nil {
 			return err
@@ -155,46 +185,105 @@ func (s *sqlite) saveScripts(c *class.Class) error {
 	return nil
 }
 
-// Load loads a class struct from the database if given a valid name.
-func (s *sqlite) Load(name string) (Item, error) {
-	c := class.New(name)
+func (s *sqlite) LoadClassByName(name string) (*storage.Class, error) {
+	class := storage.NewClass(name)
+	var err error
+	class.ID, err = s.LoadClassID(name)
+	if err != nil {
+		return nil, err
+	}
 
-	if err := s.loadID(c); err != nil {
+	if err := s.loadLabels(class); err != nil {
 		return nil, err
 	}
-	if err := s.loadLabels(c); err != nil {
+	if err := s.loadFolders(class); err != nil {
 		return nil, err
 	}
-	if err := s.loadFolders(c); err != nil {
+	if err := s.loadFiles(class); err != nil {
 		return nil, err
 	}
-	if err := s.loadFiles(c); err != nil {
-		return nil, err
-	}
-	return c, s.loadScripts(c)
+	return class, s.loadScripts(class)
 }
 
-// LoadID loads the class id.
-func (s *sqlite) LoadID(c *class.Class) error {
+func (s *sqlite) LoadClassByID(id uint) (*storage.Class, error) {
+	class := storage.NewClass("")
+	class.ID = id
+
+	if err := s.loadName(class); err != nil {
+		return nil, err
+	}
+	if err := s.loadLabels(class); err != nil {
+		return nil, err
+	}
+	if err := s.loadFolders(class); err != nil {
+		return nil, err
+	}
+	if err := s.loadFiles(class); err != nil {
+		return nil, err
+	}
+	return class, s.loadScripts(class)
+}
+
+func (s *sqlite) LoadClassID(name string) (uint, error) {
 	query := "SELECT class_id FROM class WHERE name = ?"
 
-	idRows, err := s.DB.Query(query, c.Name)
+	idRows, err := s.db.Query(query, name)
+	if err != nil {
+		return 0, err
+	}
+	defer idRows.Close()
+
+	if !idRows.Next() {
+		return 0, fmt.Errorf("could not find class %s in database", name)
+	}
+
+	var id uint
+	err = idRows.Scan(&id)
+	return id, err
+}
+
+func (s *sqlite) LoadAllClasses() ([]*storage.Class, error) {
+	query := "SELECT name FROM class ORDER BY name"
+
+	classRows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer classRows.Close()
+
+	var classes []*storage.Class
+
+	for classRows.Next() {
+		var name string
+		classRows.Scan(&name)
+		class, err := s.LoadClassByName(name)
+		if err != nil {
+			return nil, err
+		}
+		classes = append(classes, class)
+	}
+	return classes, nil
+}
+
+func (s *sqlite) loadName(class *storage.Class) error {
+	query := "SELECT name FROM class WHERE class_id = ?"
+
+	nameRows, err := s.db.Query(query, class.ID)
 	if err != nil {
 		return err
 	}
-	defer id.Close()
+	defer nameRows.Close()
 
-	if !resID.Next() {
-		return fmt.Errorf("could not find class %s in database", c.Name)
+	if !nameRows.Next() {
+		return fmt.Errorf("could not find class with id %d in database", class.ID)
 	}
-	return resID.Scan(&c.ID)
+	return nameRows.Scan(&class.Name)
 }
 
-// loadLabels loads all labels of a given class.
-func (s *sqlite) loadLabels(c *class.Class) error {
+func (s *sqlite) loadLabels(class *storage.Class) error {
 	query := "SELECT label FROM class_label WHERE class_id = ? ORDER BY label"
 
-	labelRows, err := s.DB.Query(query, c.ID)
+	labelRows, err := s.db.Query(query, class.ID)
 	if err != nil {
 		return err
 	}
@@ -203,16 +292,15 @@ func (s *sqlite) loadLabels(c *class.Class) error {
 	for labelRows.Next() {
 		var label string
 		labelRows.Scan(&label)
-		c.Labels = append(c.Labels, label)
+		class.Labels = append(class.Labels, label)
 	}
 	return nil
 }
 
-// loadFolders loads all folders of a given class.
-func (s *sqlite) loadFolders(c *class.Class) error {
+func (s *sqlite) loadFolders(class *storage.Class) error {
 	query := "SELECT target, template FROM class_folder WHERE class_id = ? ORDER BY target"
 
-	folderRows, err := s.DB.Query(query, c.ID)
+	folderRows, err := s.db.Query(query, class.ID)
 	if err != nil {
 		return err
 	}
@@ -221,16 +309,15 @@ func (s *sqlite) loadFolders(c *class.Class) error {
 	for folderRows.Next() {
 		var target, template string
 		folderRows.Scan(&target, &template)
-		c.Folders[target] = template
+		class.Folders[target] = template
 	}
 	return nil
 }
 
-// loadFiles loads all files of a given class
-func (s *sqlite) loadFiles(c *class.Class) error {
+func (s *sqlite) loadFiles(class *storage.Class) error {
 	query := "SELECT target, template FROM class_file WHERE class_id = ? ORDER BY target"
 
-	fileRows, err := s.DB.Query(query, c.ID)
+	fileRows, err := s.db.Query(query, class.ID)
 	if err != nil {
 		return err
 	}
@@ -239,16 +326,15 @@ func (s *sqlite) loadFiles(c *class.Class) error {
 	for fileRows.Next() {
 		var target, template string
 		fileRows.Scan(&target, &template)
-		c.Files[target] = template
+		class.Files[target] = template
 	}
 	return nil
 }
 
-// loadScripts loads all scripts of a given class
-func (s *sqlite) loadScripts(c *class.Class) error {
+func (s *sqlite) loadScripts(class *storage.Class) error {
 	query := "SELECT name, run_as_sudo FROM class_script WHERE class_id = ? ORDER BY run_as_sudo, name"
 
-	scriptRows, err := s.DB.Query(query, c.ID)
+	scriptRows, err := s.db.Query(query, class.ID)
 	if err != nil {
 		return err
 	}
@@ -258,7 +344,90 @@ func (s *sqlite) loadScripts(c *class.Class) error {
 		var scriptName string
 		var runAsSudo bool
 		scriptRows.Scan(&scriptName, &runAsSudo)
-		c.Scripts[scriptName] = runAsSudo
+		class.Scripts[scriptName] = runAsSudo
 	}
 	return nil
+}
+
+func (s *sqlite) RemoveClass(name string) error {
+	var err error
+
+	classID, err := s.LoadClassID(name)
+	if err != nil {
+		return err
+	}
+
+	s.tx, err = s.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Remove class and dependencies
+	if err = s.removeName(classID); err != nil {
+		return err
+	}
+	if err = s.removeLabels(classID); err != nil {
+		return err
+	}
+	if err = s.removeFolders(classID); err != nil {
+		return err
+	}
+	if err = s.removeFiles(classID); err != nil {
+		return err
+	}
+	if err = s.removeScripts(classID); err != nil {
+		return err
+	}
+	return s.tx.Commit()
+}
+
+func (s *sqlite) removeName(classID uint) error {
+	_, err := s.tx.Exec("DELETE FROM class WHERE class_id = ?", classID)
+	return err
+}
+
+func (s *sqlite) removeLabels(classID uint) error {
+	_, err := s.tx.Exec("DELETE FROM class_label WHERE class_id = ?", classID)
+	return err
+}
+
+func (s *sqlite) removeFolders(classID uint) error {
+	_, err := s.tx.Exec("DELETE FROM class_folder WHERE class_id = ?", classID)
+	return err
+}
+
+func (s *sqlite) removeFiles(classID uint) error {
+	_, err := s.tx.Exec("DELETE FROM class_file WHERE class_id = ?", classID)
+	return err
+}
+
+func (s *sqlite) removeScripts(classID uint) error {
+	_, err := s.tx.Exec("DELETE FROM class_script WHERE class_id = ?", classID)
+	return err
+}
+
+func (s *sqlite) DoesLabelExist(label string) (uint, error) {
+	query := "SELECT class_id FROM class_label WHERE label = ?"
+	var id uint
+	err := s.db.QueryRow(query, label).Scan(&id)
+	return id, err
+}
+
+func (s *sqlite) TrackProject(proj *storage.Project) error {
+	t := time.Now().Local()
+	_, err := s.db.Exec(
+		"INSERT INTO project(name, class_id, install_path, install_date, project_status_id) VALUES(?, ?, ?, ?, ?)",
+		proj.Name,
+		proj.Class.ID,
+		proj.InstallPath,
+		t,
+		1,
+	)
+
+	if sqliteErr, ok := err.(sqlite3.Error); ok {
+		if sqliteErr.Code == sqlite3.ErrConstraint {
+			return fmt.Errorf("project already exists")
+		}
+	}
+	return err
 }
--- pkg/proji/storage/storage.go
+++ pkg/proji/storage/storage.go
@@ -1,12 +1,31 @@
 package storage
 
-import (
-	"github.com/nikoksr/proji/internal/app/proji/class"
-)
-
 // Service interface describes the behaviour of a storage service.
 type Service interface {
+	// Close the interface.
 	Close() error
-	Save(*class.Class) error
-	Load(string) (*class.Class, error)
+
+	// Save a class to a storage.
+	SaveClass(class *Class) error
+
+	// Load a class from storage by its name.
+	LoadClassByName(name string) (*Class, error)
+
+	// Load a class from storage by its ID.
+	LoadClassByID(id uint) (*Class, error)
+
+	// Load the ID for a given class
+	LoadClassID(name string) (uint, error)
+
+	// Load all classes from storage
+	LoadAllClasses() ([]*Class, error)
+
+	// Remove a class from storage.
+	RemoveClass(name string) error
+
+	// Checks if a given label exists in the storage. Returns the corresponding ID if true and an error if not.
+	DoesLabelExist(label string) (uint, error)
+
+	// TrackProject adds a project to the database.
+	TrackProject(proj *Project) error
 }
