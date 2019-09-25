5abce820d8fc880bec8a05e51eb0ba0323017afb
Niko Köser <koeserniko@gmail.com>
2019-09-25 14:53:44 +0200

    Add subcommand 'ls' to basecommand 'project' Option to list all available projects would be helpful and informative.
    This change adds the subcommand and makes it list all projects with
    their important informations in a cli table.

---
+++ cmd/proji/cmd/projectLs.go
@@ -0,0 +1,61 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/jedib0t/go-pretty/table"
+	"github.com/nikoksr/proji/pkg/helper"
+	"github.com/nikoksr/proji/pkg/proji/storage/sqlite"
+	"github.com/spf13/cobra"
+)
+
+var projectLsCmd = &cobra.Command{
+	Use:   "ls",
+	Short: "list existing projects",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return ListProjects()
+	},
+}
+
+func init() {
+	projectCmd.AddCommand(projectLsCmd)
+}
+
+// ListProjects lists all available projects.
+func ListProjects() error {
+	// Setup storage service
+	sqlitePath, err := helper.GetSqlitePath()
+	if err != nil {
+		return err
+	}
+	s, err := sqlite.New(sqlitePath)
+	if err != nil {
+		return err
+	}
+	defer s.Close()
+
+	projects, err := s.ListProjects()
+	if err != nil {
+		return err
+	}
+
+	// Table header
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"ID", "Name", "Install Path", "Class", "Status"})
+
+	// Fill table
+	for _, project := range projects {
+		t.AppendRow([]interface{}{
+			project.ID,
+			project.Name,
+			project.InstallPath,
+			project.Class.Name,
+			project.Status.Title,
+		})
+	}
+
+	// Print the table
+	t.Render()
+	return nil
+}
