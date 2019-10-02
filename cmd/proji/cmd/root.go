810c084aa00305bba59839619c13150196357901
Niko Köser <koeserniko@gmail.com>
2019-10-02 23:12:14 +0200

    Add the initialization of the config path and storage service
    Prior to this change, every command created its own connection to a
    storage service. Alot of code repetition.
    
    This change initializes the config path and the storage service once.
    After the initialization the path and service are readable by all
    members of the package cmd through the package wide unexported struct
    env. In future it will be possible to simply change the storage type
    since it's now defined once and not in every subcommand.

--- cmd/proji/cmd/root.go
+++ cmd/proji/cmd/root.go
@@ -4,17 +4,41 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/nikoksr/proji/pkg/helper"
+	"github.com/mitchellh/go-homedir"
+	"github.com/nikoksr/proji/pkg/proji/storage"
+	"github.com/nikoksr/proji/pkg/proji/storage/sqlite"
 	"github.com/spf13/cobra"
-
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+// Env represents central resources and information the app uses.
+type env struct {
+	Svc      storage.Service
+	ConfPath string
+}
+
+var projiEnv *env
 
 var rootCmd = &cobra.Command{
 	Use:   "proji",
 	Short: "A lean and mean project creator and manager.",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		svc, err := initStorageService()
+		if err != nil {
+			return err
+		}
+		if projiEnv == nil {
+			return fmt.Errorf("env struct is not defined")
+		}
+		projiEnv.Svc = svc
+		return nil
+	},
+	PersistentPostRun: func(cmd *cobra.Command, args []string) {
+		if projiEnv.Svc != nil {
+			projiEnv.Svc.Close()
+			projiEnv.Svc = nil
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -31,17 +55,34 @@ func init() {
 }
 
 func initConfig() {
-	if cfgFile != "" {
-		viper.SetConfigFile(cfgFile)
-	} else {
-		viper.AddConfigPath(helper.GetConfigDir())
-		viper.SetConfigName("config")
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Printf("Could not set config path: %v", err)
+		os.Exit(1)
 	}
-
+	if projiEnv == nil {
+		projiEnv = &env{ConfPath: "", Svc: nil}
+	}
+	projiEnv.ConfPath = home + "/.config/proji/"
+	viper.AddConfigPath(projiEnv.ConfPath)
+	viper.SetConfigName("config")
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Printf("Could not read config file %s", viper.ConfigFileUsed())
 		os.Exit(1)
 	}
+}
+
+func initStorageService() (storage.Service, error) {
+	dbPath, ok := viper.Get("sqlite3.path").(string)
+	if !ok {
+		return nil, fmt.Errorf("could not read sqlite path from config file")
+	}
+
+	svc, err := sqlite.New(projiEnv.ConfPath + dbPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not connect to sqlite db: %v", err)
+	}
+	return svc, nil
 }
