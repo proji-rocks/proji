7f89a5be7ed8a915de2cac1ecd0d7e3e0c348b3e
Niko Köser <koeserniko@gmail.com>
2019-10-25 20:05:14 +0200

    Remove completion installation
    Even though commit 1663d87cadc00dc3079ec552c4edfa7aaf9567db was said to
    remove the completion, I somehow missed to actually remove the
    completion completely.
    
    This change finally removes the completion installtion completely.

--- install.sh
+++ install.sh
@@ -18,22 +18,5 @@ if ! [ -f "${CONF_DIR}examples/proji-class.toml" ]; then
     curl --silent -o "${CONF_DIR}examples/proji-class.toml" https://raw.githubusercontent.com/nikoksr/proji/master/assets/examples/example-class-export.toml
 fi
 
-# Add shell completion
-SHELL_IN_USE=$(basename $(echo "$SHELL"))
-
-if [ "$SHELL_IN_USE" = "bash" ]; then
-    ./proji completion bash >~/.config/proji/completion.bash.inc
-    printf "
-        # Proji shell completion
-        source '$HOME/.config/proji/completion.bash.inc'
-        " >>$HOME/.bash_profile
-    source $HOME/.bash_profile
-elif [ "$SHELL_IN_USE" = "zsh" ]; then
-    echo $FPATH
-    # ./proji completion zsh >"${fpath[1]}/_proji"
-else
-    echo "Shell $SHELL_IN_USE is not supported for completion."
-fi
-
 # Install the binary
 sudo install proji /usr/local/bin
