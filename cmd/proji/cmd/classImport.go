b135ece7bf02191cc29db15cd89315fb065bba6f
Niko Köser <koeserniko@gmail.com>
2019-11-17 22:37:37 +0100

    cli: add two flags to 'class import' command
    Add two flags to the 'class import command' as preperation for the
    coming support of includeable directories. The two flags are 'config'
    and 'directory'. With the 'config' flag the user will import class
    configs as before and with the 'directory' flag the user will be able to
    import a class from an existing directory. Proji will copy the structure
    and content of the directory and create a class config based on it.

--- cmd/proji/cmd/classImport.go
+++ cmd/proji/cmd/classImport.go
@@ -8,34 +8,59 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var directories, configs []string
+
 var classImportCmd = &cobra.Command{
 	Use:   "import FILE [FILE...]",
 	Short: "Import one or more classes",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return fmt.Errorf("Missing config file")
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(configs) < 1 && len(directories) < 1 {
+			return fmt.Errorf("No config or directory path was given")
 		}
-
-		for _, config := range args {
-			if err := importClass(config, projiEnv.Svc); err != nil {
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Import configs
+		for _, config := range configs {
+			if err := importClassFromConfig(config, projiEnv.Svc); err != nil {
 				fmt.Printf("> Import of '%s' failed: %v\n", config, err)
 				continue
 			}
 			fmt.Printf("> '%s' was successfully imported\n", config)
 		}
+
+		// Import directories
+		for _, config := range directories {
+			if err := importClassFromConfig(config, projiEnv.Svc); err != nil {
+				fmt.Printf("> Import of '%s' failed: %v\n", config, err)
+				continue
+			}
+			fmt.Printf("> '%s' was successfully imported\n", config)
+		}
+
 		return nil
 	},
 }
 
 func init() {
 	classCmd.AddCommand(classImportCmd)
+
+	classImportCmd.Flags().StringSliceVar(&directories, "directory", []string{}, "import/imitate an existing directory")
+	classImportCmd.MarkFlagDirname("directory")
+
+	classImportCmd.Flags().StringSliceVar(&configs, "config", []string{}, "import a class from a config file")
+	classImportCmd.MarkFlagFilename("config")
 }
 
-func importClass(config string, svc storage.Service) error {
+func importClassFromConfig(config string, svc storage.Service) error {
 	// Import class data
 	class := item.NewClass("", "", false)
-	if err := class.ImportData(config); err != nil {
+	if err := class.ImportFromConfig(config); err != nil {
 		return err
 	}
 	return svc.SaveClass(class)
+}
+
+func importClassFromDirectory(directory string, svc storage.Service) error {
+	return nil
 }
