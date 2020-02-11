0e20c0d012edbc9e1dd4422c31df4c0ae0172c29
Niko Köser <koeserniko@gmail.com>
2020-02-11 16:13:21 +0100

    refactor: update version to v0.18.1

--- cmd/proji/cmd/version.go
+++ cmd/proji/cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version of proji",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("v0.18.0")
+		fmt.Printf("v0.18.1")
 	},
 }
 
