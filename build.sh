4d552d101e0ad80406f04fd126d769784b1d6463
Niko Köser <koeserniko@gmail.com>
2020-02-25 00:02:01 +0100

    build: add mac to GOOS list

--- build.sh
+++ build.sh
@@ -4,39 +4,40 @@ PACKAGE="./cmd/proji/"
 PACKAGE_SPLIT_NAME=(${PACKAGE//\// })
 PACKAGE_NAME="${PACKAGE_SPLIT_NAME[-1]}"
 OUTPUT_PATH="bin/"
-PLATFORMS=("linux/amd64" "windows/amd64")
-# PLATFORMS=("linux/amd64" "windows/amd64" "darwin/amd64") compiling binary for darwin fails
+PLATFORMS=("darwin/amd64" "linux/amd64" "windows/amd64")
 
-echo "Cross-platform binaries for proji"
+echo
+echo " -> Building Cross-Platform Binaries"
 echo
 
 for PLATFORM in "${PLATFORMS[@]}"; do
     PLATFORM_SPLIT_NAME=(${PLATFORM//\// })
     GOOS="${PLATFORM_SPLIT_NAME[0]}"
     GOARCH="${PLATFORM_SPLIT_NAME[1]}"
-    BIN_PATH="${OUTPUT_PATH}${GOOS}/${GOARCH}/${PACKAGE_NAME}"
+    BIN_PATH="${OUTPUT_PATH}${GOOS}/${PACKAGE_NAME}"
 
-    echo " > Building ${BIN_PATH}..."
+    echo " ---> Building ${GOARCH} Binary for ${GOOS}..."
     CMD=""
 
     if [ $GOOS = "linux" ]; then
         # Linux build
-        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} go build -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
+        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} go build -a -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
     elif [ $GOOS = "windows" ]; then
         # Windows build
         BIN_PATH+='.exe'
-        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} CC=x86_64-w64-mingw32-gcc go build -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
+        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} CC=x86_64-w64-mingw32-gcc go build -a -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
     elif [ $GOOS = "darwin" ]; then
         # Mac build
-        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} CC=clang  go build -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
+        CMD="CGO_ENABLED=1 GOOS=${GOOS} GOARCH=${GOARCH} CC=o64-clang  go build -a -ldflags '-s -w' -o ${BIN_PATH} ${PACKAGE}"
     else
         echo "Error: OS not support!"
         exit 1
     fi
 
-    eval ${CMD}
+    eval ${CMD} &
     if [ $? -ne 0 ]; then
         echo "An error has occurred! Aborting the script execution..."
         exit 1
     fi
 done
+wait
