8fb6912fe58c9eafbb0ee695a1d42c1f10aaeda1
Niko Köser <koeserniko@gmail.com>
2020-05-26 14:58:34 +0200

    refactor: add ParseURL function
    ParseURL will parse repo URLs and make them comply with a standard.
    1. Possible trailing '.git' will be removed
    2. Abbreviations like 'gh:' will be replaced by th full domain; in this
       case 'https://github.com'
    3. Parse the URL string to a URL structure
    4. Check if the URL is absolute. Does it have a scheme like 'https'? If
       not, add it.

--- pkg/proji/repo/repo.go
+++ pkg/proji/repo/repo.go
@@ -3,9 +3,19 @@ package repo
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
 
 	"github.com/tidwall/gjson"
 )
+
+// domainAbbreviations defines a map of domain abbreviations like 'gh:' and their associated full domains like
+// 'https://github.com'.
+var domainAbbreviations = map[string]string{
+	"gh:": "https://github.com",
+	"gl:": "https://gitlab.com",
+}
 
 // Importer describes the behaviour of repo objects (github, gitlab)
 type Importer interface {
@@ -25,4 +35,43 @@ func GetRequest(request string) (*http.Response, error) {
 		return nil, fmt.Errorf("GET %s returned status code %d", request, response.StatusCode)
 	}
 	return response, nil
+}
+
+// ParseURL parses a regular URL of a remote repository into a "cleaned up" version.
+//
+// Steps:
+//   - Remove trailing '.git'
+//   - Replace domain abbreviations with full domains
+//   - Parse raw string to URL structure
+//   - Make absolute if not already
+func ParseURL(URL string) (*url.URL, error) {
+	// Trim trailing '.git'
+	if strings.HasSuffix(URL, ".git") {
+		URL = URL[:len(URL)-len(".git")]
+	}
+
+	// Replace domain abbreviations like 'gh:' with the actual domain of the host
+	for abbreviation, fullDomain := range domainAbbreviations {
+		if strings.HasPrefix(URL, abbreviation) {
+			URL = strings.Replace(URL, abbreviation, fullDomain, 1)
+			break
+		}
+	}
+
+	// Parse to URL structure
+	u, err := url.Parse(URL)
+	if err != nil {
+		return nil, err
+	}
+
+	// Make absolute if not already
+	if !u.IsAbs() {
+		u.Scheme = "https"
+		u, err = url.Parse(u.String())
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return u, nil
 }
