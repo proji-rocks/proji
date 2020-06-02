6b30e37f7f6f765e4cf1f1aadea444f0095e9864
Niko Köser <koeserniko@gmail.com>
2020-06-02 03:52:15 +0200

    refactor: apply changes from gitlab package to tests

--- pkg/proji/repo/gitlab/gitlab_test.go
+++ pkg/proji/repo/gitlab/gitlab_test.go
@@ -5,33 +5,29 @@ import (
 	"testing"
 
 	"github.com/nikoksr/proji/pkg/helper"
-	"github.com/nikoksr/proji/pkg/proji/repo"
 	"github.com/stretchr/testify/assert"
-	"github.com/tidwall/gjson"
 )
 
-const glAPIBase = "https://gitlab.com/api/v4/projects/"
-
-var goodURLs = []*url.URL{
-	{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/proji-test"},
-	{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/proji-test/-/tree/develop"},
+var goodRepos = []*GitLab{
+	{
+		baseURI:    &url.URL{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/proji-test"},
+		OwnerName:  "nikoksr",
+		RepoName:   "proji-test",
+		BranchName: "master",
+	},
+	{
+		baseURI:    &url.URL{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/proji-test/tree/develop"},
+		OwnerName:  "nikoksr",
+		RepoName:   "proji-test",
+		BranchName: "develop",
+	},
 }
 
-var goodRepoObjects = []repo.Importer{
-	&gitlab{
-		baseURI:    goodURLs[0],
-		apiBaseURL: glAPIBase,
-		ownerName:  "nikoksr",
-		repoName:   "proji-test",
-		branchName: "master",
-	},
-	&gitlab{
-		baseURI:    goodURLs[1],
-		apiBaseURL: glAPIBase,
-		ownerName:  "nikoksr",
-		repoName:   "proji-test",
-		branchName: "develop",
-	},
+var badURLs = []*url.URL{
+	{Scheme: "https", Host: "github.com", Path: "/nikoksr/does-not-exist"},
+	{Scheme: "https", Host: "github.com", Path: "/nikoksr/proji-test/-/tree/dead-branch"},
+	{Scheme: "https", Host: "github.com", Path: ""},
+	{Scheme: "https", Host: "google.com", Path: ""},
 }
 
 // TestNew tests the creation of a new github object based on given github URLs.
@@ -39,113 +35,211 @@ func TestNew(t *testing.T) {
 	helper.SkipNetworkBasedTests(t)
 
 	// These should work
-	for i, URL := range goodURLs {
-		g, err := New(URL)
+	for i, repo := range goodRepos {
+		g, err := New(repo.baseURI)
 		assert.NoError(t, err)
 		assert.NotNil(t, g)
-		assert.Equal(t, goodRepoObjects[i], g)
+		assert.Equal(t, goodRepos[i].baseURI, g.baseURI)
+		assert.Equal(t, goodRepos[i].OwnerName, g.OwnerName)
+		assert.Equal(t, goodRepos[i].RepoName, g.RepoName)
+		assert.Equal(t, goodRepos[i].BranchName, g.BranchName)
 	}
 
 	// These should fail
-	var badURLs = []*url.URL{
-		{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/does-not-exist"},
-		{Scheme: "https", Host: "gitlab.com", Path: "/nikoksr/proji-test/-/tree/dead-branch"},
-		{Scheme: "https", Host: "gitlab.com", Path: ""},
-		{Scheme: "https", Host: "github.com", Path: "/nikoksr/blaa"},
-		{Scheme: "https", Host: "google.com", Path: ""},
-	}
-
 	for _, URL := range badURLs {
-		glRepo, err := New(URL)
-		if err == nil {
-			_, _, err = glRepo.GetTree(nil)
-		}
+		_, err := New(URL)
 		assert.Error(t, err)
 	}
 }
 
-// TestGetTreePathsAndTypes tests the github method TestGetTreePathsAndTypes which tries
+// TestGitLab_LoadTreeEntries tests the github method TestGetTreePathsAndTypes which tries
 // to request and receive the folders paths and types of a github repo tree.
-func TestGetTreePathsAndTypes(t *testing.T) {
+func TestGitLab_LoadTreeEntries(t *testing.T) {
 	helper.SkipNetworkBasedTests(t)
 
-	var goodRepoPathsResults = [][]gjson.Result{
+	type testEntry struct {
+		id        string
+		path      string
+		entryType string
+	}
+
+	tests := []struct {
+		URL         *url.URL
+		wantErr     bool
+		treeEntries []*testEntry
+	}{
 		{
-			{Type: gjson.Type(3), Raw: "\".vscode\"", Str: ".vscode", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"include\"", Str: "include", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src\"", Str: "src", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"test\"", Str: "test", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/c_cpp_properties.json\"", Str: ".vscode/c_cpp_properties.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/launch.json\"", Str: ".vscode/launch.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/tasks.json\"", Str: ".vscode/tasks.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"CMakeLists.txt\"", Str: "CMakeLists.txt", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"README.md\"", Str: "README.md", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"include/helper.hpp\"", Str: "include/helper.hpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src/helper.cpp\"", Str: "src/helper.cpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src/main.cpp\"", Str: "src/main.cpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"test/TestHelper.cpp\"", Str: "test/TestHelper.cpp", Num: 0, Index: 0},
+			URL:     goodRepos[0].baseURI,
+			wantErr: false,
+			treeEntries: []*testEntry{
+				{
+					id:        "ce67480c3cd24e7dd675a7486233231c050f2c2e",
+					path:      ".vscode",
+					entryType: "tree",
+				},
+				{
+					id:        "7213500b0fd381eb9c8e57cfdfd9b0387bcabce0",
+					path:      "include",
+					entryType: "tree",
+				},
+				{
+					id:        "f22f80dfb366d311404859100709fcc348668aff",
+					path:      "src",
+					entryType: "tree",
+				},
+				{
+					id:        "1c0a24c11ed67d83dec1cc26d252ab3d52da9f3f",
+					path:      "test",
+					entryType: "tree",
+				},
+				{
+					id:        "5de84ef9d7019f8b47493e5d111dc1d60cf7a452",
+					path:      ".vscode/c_cpp_properties.json",
+					entryType: "blob",
+				},
+				{
+					id:        "cf646956cf7745868f005a3b0fc622fa0390b3d7",
+					path:      ".vscode/launch.json",
+					entryType: "blob",
+				},
+				{
+					id:        "ecbd3b5084f7657eea227f09e8fe5c0972d98d0b",
+					path:      ".vscode/tasks.json",
+					entryType: "blob",
+				},
+				{
+					id:        "a16196bf1875a1054b731e47c528bdfc828c0649",
+					path:      "CMakeLists.txt",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "README.md",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "include/helper.hpp",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "src/helper.cpp",
+					entryType: "blob",
+				},
+				{
+					id:        "b3cf51681c44016f9234f67dbd00ee49704b0021",
+					path:      "src/main.cpp",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "test/TestHelper.cpp",
+					entryType: "blob",
+				},
+			},
 		},
 		{
-			{Type: gjson.Type(3), Raw: "\".vscode\"", Str: ".vscode", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"include\"", Str: "include", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src\"", Str: "src", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"test\"", Str: "test", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/c_cpp_properties.json\"", Str: ".vscode/c_cpp_properties.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/launch.json\"", Str: ".vscode/launch.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\".vscode/tasks.json\"", Str: ".vscode/tasks.json", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"CMakeLists.txt\"", Str: "CMakeLists.txt", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"README.md\"", Str: "README.md", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"include/helper.hpp\"", Str: "include/helper.hpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"notes.txt\"", Str: "notes.txt", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src/helper.cpp\"", Str: "src/helper.cpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"src/main.cpp\"", Str: "src/main.cpp", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"test/TestHelper.cpp\"", Str: "test/TestHelper.cpp", Num: 0, Index: 0},
+			URL:     goodRepos[1].baseURI,
+			wantErr: false,
+			treeEntries: []*testEntry{
+				{
+					id:        "ce67480c3cd24e7dd675a7486233231c050f2c2e",
+					path:      ".vscode",
+					entryType: "tree",
+				},
+				{
+					id:        "7213500b0fd381eb9c8e57cfdfd9b0387bcabce0",
+					path:      "include",
+					entryType: "tree",
+				},
+				{
+					id:        "f22f80dfb366d311404859100709fcc348668aff",
+					path:      "src",
+					entryType: "tree",
+				},
+				{
+					id:        "1c0a24c11ed67d83dec1cc26d252ab3d52da9f3f",
+					path:      "test",
+					entryType: "tree",
+				},
+				{
+					id:        "5de84ef9d7019f8b47493e5d111dc1d60cf7a452",
+					path:      ".vscode/c_cpp_properties.json",
+					entryType: "blob",
+				},
+				{
+					id:        "cf646956cf7745868f005a3b0fc622fa0390b3d7",
+					path:      ".vscode/launch.json",
+					entryType: "blob",
+				},
+				{
+					id:        "ecbd3b5084f7657eea227f09e8fe5c0972d98d0b",
+					path:      ".vscode/tasks.json",
+					entryType: "blob",
+				},
+				{
+					id:        "a16196bf1875a1054b731e47c528bdfc828c0649",
+					path:      "CMakeLists.txt",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "README.md",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "include/helper.hpp",
+					entryType: "blob",
+				},
+				{
+					id:        "77083e0ec310487f88cf875f5ea7f377ee1819ad",
+					path:      "notes.txt",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "src/helper.cpp",
+					entryType: "blob",
+				},
+				{
+					id:        "b3cf51681c44016f9234f67dbd00ee49704b0021",
+					path:      "src/main.cpp",
+					entryType: "blob",
+				},
+				{
+					id:        "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+					path:      "test/TestHelper.cpp",
+					entryType: "blob",
+				},
+			},
+		},
+		{
+			URL:         badURLs[0],
+			wantErr:     true,
+			treeEntries: nil,
 		},
 	}
 
-	var goodRepoTypesResults = [][]gjson.Result{
-		{
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-		},
-		{
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"tree\"", Str: "tree", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-			{Type: gjson.Type(3), Raw: "\"blob\"", Str: "blob", Num: 0, Index: 0},
-		},
-	}
+	for _, test := range tests {
+		g, err := New(test.URL)
+		assert.Equal(t, err != nil, test.wantErr, "LoadTreeEntries() error = %v, wantErr %v", err, test.wantErr)
 
-	// These should work
-	for i, ghRepo := range goodRepoObjects {
-		paths, types, err := ghRepo.GetTree(nil)
-		assert.NoError(t, err)
-		assert.NotNil(t, paths)
-		assert.NotNil(t, types)
-		assert.Equal(t, goodRepoPathsResults[i], paths)
-		assert.Equal(t, goodRepoTypesResults[i], types)
+		if g == nil {
+			continue
+		}
+
+		err = g.LoadTreeEntries()
+		assert.Equal(t, err != nil, test.wantErr, "LoadTreeEntries() error = %v, wantErr %v", err, test.wantErr)
+
+		for i, entry := range g.TreeEntries {
+			assert.Equal(t, entry.ID, test.treeEntries[i].id)
+			assert.Equal(t, entry.Path, test.treeEntries[i].path)
+			assert.Equal(t, entry.Type, test.treeEntries[i].entryType)
+		}
 	}
+}
 
 	// These should fail
 	var badRepoObjects = []repo.Importer{
