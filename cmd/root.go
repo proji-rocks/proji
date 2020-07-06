5150beb30cb0d6c457b0c566a16da71287f6e6c9
Niko Köser <koeserniko@gmail.com>
2020-07-07 01:12:39 +0200

    chore: remove redundant error printing

--- cmd/root.go
+++ cmd/root.go
@@ -52,10 +52,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		log.Fatal(err)
-	}
+	_ = rootCmd.Execute()
 }
 
 func init() {
