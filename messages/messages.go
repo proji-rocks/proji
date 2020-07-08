a4b818f42d839af66155aa8ad117970d23bd1ad5
Niko Köser <koeserniko@gmail.com>
2020-07-08 21:52:48 +0200

    refactor: add messages package
    Messages provides a couple of wrapper functions for info, success,
    warning and error messages. Info, success and warning are printed to
    stdout and error is printed to stderr.
    All wrappers use a custom prefix and colors to prettify the messages.

---
+++ messages/messages.go
@@ -0,0 +1,96 @@
+package messages
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/gookit/color"
+)
+
+const (
+	symbolInfo    = "• "
+	symbolSuccess = "🗸 "
+	symbolWarning = "⚡"
+	symbolError   = "✗ "
+)
+
+var (
+	defaultOutput      = os.Stdout
+	defaultErrorOutput = os.Stderr
+
+	defaultColorNone    = color.FgDefault.Render
+	defaultColorInfo    = color.FgBlue.Render
+	defaultColorSuccess = color.FgGreen.Render
+	defaultColorWarning = color.FgYellow.Render
+	defaultColorError   = color.FgRed.Render
+
+	colorInfo    = defaultColorInfo
+	colorSuccess = defaultColorSuccess
+	colorWarning = defaultColorWarning
+	colorError   = defaultColorError
+
+	prefixInfo    = colorInfo(symbolInfo)
+	prefixSuccess = colorSuccess(symbolSuccess)
+	prefixWarning = colorWarning(symbolWarning)
+	prefixError   = colorError(symbolError)
+)
+
+func EnableColors(disableColors bool) {
+	if disableColors {
+		setNoColors()
+		renderPrefixes()
+	}
+}
+
+func setNoColors() {
+	colorInfo = defaultColorNone
+	colorSuccess = defaultColorNone
+	colorWarning = defaultColorNone
+	colorError = defaultColorNone
+}
+
+func renderPrefixes() {
+	prefixInfo = colorInfo(symbolInfo)
+	prefixSuccess = colorSuccess(symbolSuccess)
+	prefixWarning = colorWarning(symbolWarning)
+	prefixError = colorError(symbolError)
+}
+
+// Info prints a formatted information message.
+func Info(format string, args ...interface{}) {
+	printPrefixedMessage(defaultOutput, prefixInfo, format, args...)
+}
+
+// Success prints a formatted success message.
+func Success(format string, args ...interface{}) {
+	printPrefixedMessage(defaultOutput, prefixSuccess, format, args...)
+}
+
+// Warning prints a formatted warning message.
+func Warning(format string, args ...interface{}) {
+	printPrefixedMessage(defaultOutput, prefixWarning, format, args...)
+}
+
+// Error prints a formatted error message.
+func Error(format string, err error, args ...interface{}) {
+	format = formatErrorMessage(format, err)
+	printPrefixedMessage(defaultErrorOutput, prefixError, format, args...)
+}
+
+func formatErrorMessage(format string, err error) string {
+	format = strings.TrimSpace(format)
+	if len(format) < 1 {
+		return colorError("error") + "=" + err.Error()
+	}
+	return format + " " + colorError("error") + "=" + err.Error()
+}
+
+func printPrefixedMessage(out io.Writer, prefix, format string, args ...interface{}) {
+	_, err := fmt.Fprintf(out, prefix+format+"\n", args...)
+	if err != nil {
+		log.Fatalf("failed to print message to %v.\n", out)
+	}
+}
