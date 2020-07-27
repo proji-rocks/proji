c61c74be4e0e8d8686c31f88793dac33b0c248d7
Niko Köser <koeserniko@gmail.com>
2020-07-27 23:36:35 +0200

    refactor(store): add sql transactions to project store

--- pkg/project/store/store.go
+++ pkg/project/store/store.go
@@ -1,9 +1,8 @@
 package projectstore
 
 import (
-	"errors"
-
 	"github.com/nikoksr/proji/pkg/domain"
+	"github.com/pkg/errors"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
@@ -23,15 +22,33 @@ func (ps *projectStore) StoreProject(project *domain.Project) error {
 	if err == nil {
 		return ErrProjectExists
 	}
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return ps.db.Create(project).Error
+	if !errors.Is(err, gorm.ErrRecordNotFound) {
+		return err
 	}
-	return err
+
+	tx := ps.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if tx.Error != nil {
+		tx.Rollback()
+		return tx.Error
+	}
+
+	err = tx.Omit(clause.Associations).Create(project).Error
+	if err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "insert project")
+	}
+
+	return tx.Commit().Error
 }
 
 func (ps *projectStore) LoadProject(path string) (*domain.Project, error) {
 	var project domain.Project
-	tx := ps.db.Preload(clause.Associations).Where("path = ?", path).First(&project)
+	tx := ps.db.Where("path = ?", path).First(&project)
 	if errors.Is(tx.Error, gorm.ErrRecordNotFound) || tx.RowsAffected < 1 {
 		return nil, ErrProjectNotFound
 	}
@@ -64,17 +81,46 @@ func (ps *projectStore) loadAllProjects() ([]*domain.Project, error) {
 }
 
 func (ps *projectStore) UpdateProjectLocation(oldPath, newPath string) error {
-	tx := ps.db.Model(&domain.Project{Path: oldPath}).Update("path", newPath)
-	if errors.Is(tx.Error, gorm.ErrRecordNotFound) || tx.RowsAffected < 1 {
+	tx := ps.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if err := tx.Error; err != nil {
+		return err
+	}
+
+	err := tx.Model(&domain.Project{Path: oldPath}).Update("path", newPath).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) || tx.RowsAffected < 1 {
+		tx.Rollback()
 		return ErrProjectNotFound
 	}
-	return tx.Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
 }
 
 func (ps *projectStore) RemoveProject(path string) error {
-	tx := ps.db.Set("gorm:delete_option", "OPTION (OPTIMIZE FOR UNKNOWN)").Where("path = ?", path).Delete(&domain.Project{})
-	if errors.Is(tx.Error, gorm.ErrRecordNotFound) || tx.RowsAffected < 1 {
+	tx := ps.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+	if err := tx.Error; err != nil {
+		return err
+	}
+	err := tx.Set("gorm:delete_option", "OPTION (OPTIMIZE FOR UNKNOWN)").Where("path = ?", path).Delete(&domain.Project{}).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		tx.Rollback()
 		return ErrProjectNotFound
 	}
-	return tx.Error
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
 }
